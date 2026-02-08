285d34ba3a2e6c2650fdb643b429e13a7833afd2
CC3349 <chahat.chopra1556@gmail.com>
2026-02-08 13:51:16 +0530

    completed loops

--- basics/loops.cpp
+++ basics/loops.cpp
@@ -33,25 +33,117 @@ int main(){
 
 
 //how to use break
-int n=50;
-int sum=0;
-
-for(int i=1; i<=n; i++){
-sum += i;
-if(i==7){
-break;// special reserved word -> keyword
-}
-}
-cout<<sum;
-
-
-
-//sum of all odd no.s from 1 to n
+//int n=50;
+//int sum=0;
+//
+//for(int i=1; i<=n; i++){
+//sum += i;
+//if(i==7){
+//break;// special reserved word -> keyword
+//}
+//}
+//cout<<sum;
 
 
 
+//sum of all odd no.s from 1 to n using for
+//int n=5;
+//int sum=0;
+//for(int i=0;i<=n;i++){
+//if(i%2!=0){//if even i%2==0
+//sum += i;
+//}
+//}
+//cout<<sum<<" ";
+
+//using while loop
+//int n=5;
+//int sum=0;
+//int i=0;
+//while(i<=n){//yes
+//if(i%2!=0){//no
+//sum += i;
+//
+//}
+//i++;
+//}
+//cout<<sum<<" ";
+
+
+//do while loop
+//do{
+//}while(condn)
+//kaam pehle hota hai condn baadmein check hoti hai
+//toh agar condn false hoogi toh bhi loop ek baar toh chalega
+
+//do{
+//cout<<"heloooo";
+//}while(3>5);
+//
+
+
+//check if a no.is prime or not
+//int n;
+//bool isprime = true;
+//cout<<"n?"<<endl;
+//cin>>n;
+//for(int i=2;i*i<=n;i++){
+//if(n%i==0){//non prime
+//isprime = false;
+//break;
+//}}
+//if(isprime == true){
+//cout<<"prime no";
+//}else{
+//cout<<"not prime";
+//}
 
 
 
+//nested loops->loop inside loop
+//for(int i=1;i<=5;i++){
+//cout<<"*****"<<endl;;
+//}
+//*****
+//*****
+//*****
+//*****
+//*****
+
+//int x=10;
+//for(int i=1;i<=x;i++){
+//cout<<"*";}
+//**********
+
+
+//int x=10;
+//for(int i=1;i<=x;i++){
+//cout<<"*";}
+//cout<<endl;
+//**********
+
+//final
+//for(int i=1;i<=6;i++){//column
+//for(int j=1;j<=5;j++){//row
+//cout<<"*";}
+//cout<<endl;}
+
+
+
+// sum of all no.s from 1 to n which are divisible by 3
+//int n=10;
+//int sum = 0;
+//for(int i=0;i<=n;i++){
+//if(i%3==0){
+//sum += i;}
+//}
+//cout<<sum<<" ";
+
+//print factorial of a number n
+//int n=4;
+//int factorial=1;
+//for(int i=1;i<=n;i++){
+//factorial *= i;}
+//cout<<factorial<<" ";
 return 0;
 }
